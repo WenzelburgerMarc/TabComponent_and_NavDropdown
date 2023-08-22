288066422183ea330bf5e629140ee3557c715a74
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2023-08-22 18:10:03 +0200

    Navbar Dropdown Finished also mobile

---
+++ src/components/Navbar/Items/MobileToggleBtn.vue
@@ -0,0 +1,14 @@
+<template >
+    <button type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg sm:hidden focus:outline-none hover:bg-gray-600"
+        aria-controls="navbar-default">
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+    </button>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped></style>
--- src/components/Navbar/TheNavbar.vue
+++ src/components/Navbar/TheNavbar.vue
@@ -1,15 +1,56 @@
 <template>
     <div class="navbar fixed top-0 left-o w-screen h-20 bg-slate-700 flex justify-between items-center">
-        <nav-brand-item class="mr-auto" />
-        <nav-item class="mr-5" />
-        <nav-item class="mr-5" />
-        <nav-item class="mr-5" />
-        <nav-dropdown-item class="mr-5" />
+        <nav-brand-item class="mr-auto relative" />
+
+        <mobile-toggle-btn v-if="isMobile" @click="toggleMobile" class="fixed right-3" />
+
+        <transition name="translate-down" mode="out-in">
+            <div :key="isMobileActive.toString()"
+                :class="[isMobileActive ? '' : 'hidden', isMobile ? 'translate-y-20' : 'translate-y-0']"
+                class="item-container flex flex-col items-center sm:flex-row bg-slate-700 p-1 rounded-b-md mt-auto sm:mt-0">
+
+                <nav-item class="mr-5" />
+                <nav-item class="mr-5" />
+                <nav-item class="mr-5" />
+                <nav-dropdown-item />
+
+            </div>
+        </transition>
+
     </div>
 </template>
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import NavBrandItem from './Items/NavBrandItem.vue';
 import NavItem from './Items/NavItem.vue';
 import NavDropdownItem from './Items/NavDropdownItem.vue';
+import MobileToggleBtn from './Items/MobileToggleBtn.vue';
+
+let isMobileActive = ref(true);
+let isMobile = ref(false);
+
+function checkIsMobile() {
+    isMobile.value = window.innerWidth < 640;
+
+    if (!isMobile.value) {
+        isMobileActive.value = true;
+    } else {
+        isMobileActive.value = false;
+    }
+
+}
+
+window.addEventListener("resize", checkIsMobile);
+
+
+onMounted(() => {
+    checkIsMobile();
+});
+
+
+function toggleMobile() {
+    isMobileActive.value = !isMobileActive.value;
+}
+
 </script>
 <style scoped></style>
